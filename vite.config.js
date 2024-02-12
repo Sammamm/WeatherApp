66947dcb7acad1c40a22da3369ef06addadf9dff
Sammamm <135961382+Sammamm@users.noreply.github.com>
2024-02-12 14:01:32 +0530

    update to include vitest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    css: true,
+  },
+  esbuild: {
+    loader: "jsx",
+    include: [
+      // Add this for business-as-usual behaviour for .jsx and .tsx files
+      "src/**/*.jsx",
+      "src/**/*.tsx",
+      "node_modules/**/*.jsx",
+      "node_modules/**/*.tsx",
+
+
+      // --- OR ---
+
+      // Add these lines to allow all .js files to contain JSX
+      "src/**/*.js",
+      "node_modules/**/*.js",
+
+      // Add these lines to allow all .ts files to contain JSX
+      "src/**/*.ts",
+      "node_modules/**/*.ts",
+    ],
+    exclude: [],
+  },
+  resolve: {
+    extensions: [".web.tsx", ".web.jsx", ".web.js", ".tsx", ".ts", ".js", ".jsx"],
+    preserveSymlinks: true, 
+  }
 })
