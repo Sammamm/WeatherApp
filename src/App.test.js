2395100d2669ec1b1db067329fed2675d45db0e5
Sammamm <135961382+Sammamm@users.noreply.github.com>
2024-02-12 14:00:35 +0530

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,60 @@
+import { fireEvent, getByPlaceholderText, getByTestId, render, screen } from "@testing-library/react";
+import App, { loadOptions } from "./App";
+import { describe, expect, test, vi } from "vitest";
+import Search from "./components/search/search";
+
+const mockOnSearchChange = vi.fn(() => {
+  console.log("Search value:", "asdasdasd");
+});
+
+const mockLoadOptions = vi.fn((inputValue) => {
+  return {
+    options: [
+      { value: "1", label: "City 1" },
+      { value: "2", label: "City 2" },
+    ],
+  };
+});
+
+describe("App file (App.jsx) testing", () => {
+  const seachData = {};
+  it(
+    ("Testing 'loadOptions' to be defined",
+    () => {
+      render(<App />);
+      expect(loadOptions).toBeDefined();
+    })
+  );
+
+  it(
+    ("Testing 'OnSearch' to be defined",
+    () => {
+      render(<App />);
+      expect(onSearchChange).toBeDefined();
+    })
+  );
+});
+
+describe("Searchbar (Searh.jsx) testing", () => {
+  
+
+  it("Searching test", () => {
+    render(
+      <Search
+        search={null}
+        onSearchChange={mockOnSearchChange}
+        loadOptions={mockLoadOptions}
+      />
+    );
+    
+    const searchbar = screen.getByTestId("searchbar"); // Using screen.getByTestId instead of getByTestId
+    expect(searchbar).toBeInTheDocument();
+
+  });
+  
+  it('Test the Async Paginate', () => {
+    const asyncPaginate = screen.getByTestId("async-paginate");
+    fireEvent.change(asyncPaginate, { target: { value: 'New York' } });
+    expect(mockOnSearchChange).toHaveBeenCalledWith('New York');
+  })
+});
